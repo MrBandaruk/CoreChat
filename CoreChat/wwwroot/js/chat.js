02093633f1f7bc63a58291a32b67bc5bb04b30f4
Nikita Bandaruk <nbandaruk@gmail.com>
2018-06-28 14:27:32 +0300

    Add project files.

---
+++ CoreChat/wwwroot/js/chat.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    var userName = localStorage.getItem("Username") || "user";
+
+    var protocol = location.protocol === "https:" ? "wss:" : "ws:";
+    var wsUri = protocol + "//" + window.location.host;
+    console.log(wsUri);
+    var socket = new WebSocket(wsUri);
+    socket.onopen = e => {
+        console.log("socket opened", e);
+    };
+
+    socket.onclose = function (e) {
+        console.log("socket closed", e);
+    };
+
+    socket.onmessage = function (e) {
+        console.log(e);
+        $('#msgs').append(`<p class="message">${e.data}</p>`);
+    };
+
+    socket.onerror = function (e) {
+        console.error(e.data);
+    };
+
+    $('#msgInput').keypress(function (e) {
+        if (e.which != 13) {
+            return;
+        }
+
+        e.preventDefault();
+
+        var msgStr = $('#msgInput').val();
+        if (msgStr) {
+            var message = userName + ": " + msgStr;
+            socket.send(message);
+        }
+
+        $('#msgInput').val('');
+    });
+});
